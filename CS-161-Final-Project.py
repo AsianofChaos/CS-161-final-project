42a45a6746abe4cc4301a963b8eaa5e36f8b526b
Joshua Isakson <Joshuabisakson@gmail.com>
2021-06-06 02:34:42 -0700

    Create CS-161-Final-Project.py

---
+++ CS-161-Final-Project.py
@@ -0,0 +1,18 @@
+#Joshua Isakson
+#6/5/2021
+white = 'white'
+black = 'black'
+
+class Rules:
+    def self:
+        self.turn = white
+
+class piece:
+    def __init__ :
+        self.name = name
+        self.position = None
+        self.color = color
+
+
+
+class king()
